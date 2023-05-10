00803c36a76f167fe9ace36b887bd5c92378b210
lukian12 <125692918+lukian12@users.noreply.github.com>
2023-05-10 16:26:06 -0300

    Add files via upload

---
+++ js.2/javas_cript/script.js
@@ -0,0 +1,11 @@
+function gerar() {
+    const qtd = parseint(document.getElementById("gerar").Value);
+var box;
+for(let k=0; k<qtd; k++){
+    box = document.createElement("box");
+    box.innerHTML = K;
+    box.setAttribute("class","box");
+
+    document.body.appendChild(divbox);
+}
+}
